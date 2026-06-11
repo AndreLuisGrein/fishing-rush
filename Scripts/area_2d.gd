65e4f0b3904b933cdb6cd62a54681972674867d3
Guigo-dev <guipadiferreira@gmail.com>
2026-06-10 21:10:44 -0300

    spawn automatico dos peixes

--- Scripts/area_2d.gd
+++ Scripts/area_2d.gd
@@ -1,5 +1,5 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Peixe":
+	if body.is_in_group("peixes"):
 		body.direction *= -1
--- Scripts/peixe.gd
+++ Scripts/peixe.gd
@@ -5,14 +5,29 @@ var direction = 1
 var rng = RandomNumberGenerator.new()
 
 @onready var spritePeixe = $SpritePeixe
+@onready var colissionPeixe = $ColissionPeixe
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	add_to_group("peixes")
 	var numeroPeixe = rng.randi_range(1,5)
 	var caminhoTexturePeixe = "res://PeixesImages/Peixe" + str(numeroPeixe)+ ".png"
 	spritePeixe.texture = load(caminhoTexturePeixe)
+	DefinirColission()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	linear_velocity = Vector2(SPEED * direction, linear_velocity.y)
+	if linear_velocity.x < 0:
+		spritePeixe.flip_h = false
+	elif linear_velocity.x > 0:
+		spritePeixe.flip_h = true
+		
+		
+func DefinirColission():
+	var shape = RectangleShape2D.new()
+	var tamanho = spritePeixe.texture.get_size()
+	shape.size = tamanho
+	colissionPeixe.shape = shape
+	
